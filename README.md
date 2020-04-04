3bb1080a78974f7b096b723ea457ecf1d2b3ece4
Matt Cosand <matt@cosand.net>
2020-04-03 20:35:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+kcsara-exams
---
+++ kcsara-exams/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kcsara_exams.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
